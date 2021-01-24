c8d42a25ede716f069c28a0ba3d6dbf0eb82f9b9
rabisse <rabisse@gmail.com>
2021-01-24 14:48:43 +0000

    Done with add guest option

--- run_program.py
+++ run_program.py
@@ -2,7 +2,6 @@ from classes.guest import Guest
 from classes.room import Room
 from classes.song import Song
 
-
 room = Room("Night at the Roxbury Room", 2, 20)
 
 song_1 = Song("What Is Love", "Haddaway")
@@ -12,18 +11,19 @@ guest_1 = Guest("Steve Butabi", 100, "What Is Love")
 guest_2 = Guest("Doug Butabi", 80, "Stayin Alive")
 guest_3 = Guest("Malcolm Holwill", 10, "What Is Love")
 guest_4 = Guest("Katie Jeffree",50, "Stayin Alive")
+guest_list = [guest_1, guest_2, guest_3, guest_4]
 
 
 def print_menu():
-    print("CodeClan Caraoke - Night at the Roxbury Room:")
+    print("\nCodeClan Caraoke - Night at the Roxbury Room:")
     print("1: Room Status")
-    # print("2: Get Uncompleted Tasks")
+    print("2: Add Guest")
     # print("3: Get Completed Tasks")
     # print("4: Mark Task as Complete")
     # print("5: Get Tasks Which Take Longer Than a Given Time")
     # print("6: Find Task by Description")
     # print("7: Add a new Task to list")
-    print("Q or q: Quit")
+    print("(Q)uit")
 
 while (True):
     print_menu()
@@ -32,15 +32,33 @@ while (True):
         break
 
     if option == '1':
-        print(f"\n{room.name}: \nCapacity: {room.capacity} \nOccupants: {room.occupants} \nSong Queue: {room.song_queue} \nBill: {room.bill} \n")
-        print
+        print(f"\n{room.name}: \nCapacity: {room.capacity} \nOccupants: {room.num_of_occupants()} {[guest.name for guest in room.occupants]} \nSong Queue: {room.song_queue} \nBill: {room.bill} ")
+
+    elif option == '2':
+        print(f"\nGuests: \n1: {guest_list[0].name} \n2: {guest_list[1].name} \n3: {guest_list[2].name} \n4: {guest_list[3].name}")     # what is best way to do this for a long guest list? Using index means that removing a guest from list when added returns an error when trying to list it again
+        guest_selection = input("Which of the above guests: \n")
+        if (guest_selection == '1'):
+#            room.add_guest_to_room(guest_1)
+            guest_list.remove(guest_1)
+        elif (guest_selection == '2'):
+            room.add_guest_to_room(guest_2)
+#            guest_list.remove(guest_2)
+        elif (guest_selection == '3'):
+            room.add_guest_to_room(guest_3)
+#            guest_list.remove(guest_3)
+        elif (guest_selection == '4'):
+            room.add_guest_to_room(guest_4)
+#            guest_list.remove(guest_4)
+        else:
+            print("Invalid Input - choose another option")
+
+
+    elif option == '3':
+        
 
 
 
-    # elif option == '2':
-    #     print_list(get_uncompleted_tasks(tasks))
-    # elif option == '3':
-    #     print_list(get_completed_tasks(tasks))
+
     # elif option == '4':
     #     description = input("Enter task description to search for: ")
     #     task = get_task_with_description(tasks, description)
